8263a0e5a3f49c7bdbdb286cc4d986ca154b99c2
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-04-02 00:13:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ end
 def summon_captain_planet(planeteer_calls)
   calls = []
   planeteer_calls.collect do |call|
-    elements = call.capitlaize + "!"
+    elements = call.capitlize + "!"
     calls << elements
   end
   return calls
